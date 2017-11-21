11489c4ef48015a25d09a155d3c6b5876aaf5509
Di Luigi Marco <diluigimarco99@gmail.com>
2017-11-21 04:28:02 +0100

    Committing file Main

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int c = 0;
+     for (int i=0;i<10;i++)
+     {
+         c++;
+         cout <<"C = " << c << endl;
+     }
+     system ("PAUSE");
+}
